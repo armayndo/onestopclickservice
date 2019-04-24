0db7399f851527d5cb327a4fd773c06b215907be
Syarif Hidayat <Syarif_H657@mitrais.com>
2019-04-24 09:15:46 +0700

    add register service to register new user

--- src/main/java/com/osc/server/service/UserService.java
+++ src/main/java/com/osc/server/service/UserService.java
@@ -1,6 +1,26 @@
 package com.osc.server.service;
 
 import com.osc.server.model.User;
+import com.osc.server.repository.IBaseRepository;
+import com.osc.server.repository.IUserRepository;
+
+import static org.springframework.http.ResponseEntity.ok;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.DisabledException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 /**
@@ -9,5 +29,42 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/v1/users")
 public class UserService extends BaseService<User> {
+	
+	@Autowired
+    private IUserRepository repository;
+	
+	private Logger logger = LoggerFactory.getLogger(UserService.class);
+	
+	/*This service use to register new user using form-data format from frontend WEB UI*/
+	@RequestMapping(value="/register", method=RequestMethod.POST, consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+	public ResponseEntity<Map> signin(HttpServletRequest request) {
+
+		Map<Object, Object> model = new HashMap<>();
+		User user = new User();
+				 
+		try {
+			String username = request.getParameter("username");
+			if(repository.findByUsername(username) == null) {
+				user.setUsername(username);
+				user.setPassword(new BCryptPasswordEncoder().encode(request.getParameter("password")));
+				user.setFirstName(request.getParameter("firstname"));
+				user.setLastName(request.getParameter("lastname"));	
+				user.setEmail(request.getParameter("email"));
+				user.setRole(request.getParameter("role"));	
+				logger.info("Enabled: "+Boolean.parseBoolean(request.getParameter("enabled")));
+				user.setEnabled(Boolean.parseBoolean(request.getParameter("enabled")));
+				repository.save(user);
+		        model.put("user", user);
+			}else
+			{
+				model.put("Error", "username is not available, please find the new one");
+			}
+				                
+	    } catch (Exception e) {
+	    	model.put("Error", e.getMessage());
+	    }
+		 
+		return ok(model);
+	 }
 }
 
