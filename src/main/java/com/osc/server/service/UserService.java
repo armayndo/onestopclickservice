88b790b206b97e1dd9f5abf4d723d12d750b3f72
Syarif Hidayat <Syarif_H657@mitrais.com>
2019-04-26 07:50:20 +0700

    add user update service

--- src/main/java/com/osc/server/service/UserService.java
+++ src/main/java/com/osc/server/service/UserService.java
@@ -4,6 +4,7 @@ import com.osc.server.model.User;
 import com.osc.server.repository.IBaseRepository;
 import com.osc.server.repository.IUserRepository;
 
+
 import static org.springframework.http.ResponseEntity.ok;
 
 import java.util.HashMap;
@@ -31,10 +32,13 @@ import org.springframework.web.bind.annotation.*;
 public class UserService extends BaseService<User> {
 	
 	@Autowired
-    private IUserRepository repository;
+	private IUserRepository repository;
 	
 	private Logger logger = LoggerFactory.getLogger(UserService.class);
 	
+	/**
+	 * Created by Syarif Hidayat on 24/04/2019.
+	*/
 	/*This service use to register new user using form-data format from frontend WEB UI*/
 	@RequestMapping(value="/register", method=RequestMethod.POST, consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
 	public ResponseEntity<Map> signin(HttpServletRequest request) {
@@ -50,9 +54,10 @@ public class UserService extends BaseService<User> {
 				user.setFirstName(request.getParameter("firstname"));
 				user.setLastName(request.getParameter("lastname"));	
 				user.setEmail(request.getParameter("email"));
-				user.setRole(request.getParameter("role"));	
-				logger.info("Enabled: "+Boolean.parseBoolean(request.getParameter("enabled")));
-				user.setEnabled(Boolean.parseBoolean(request.getParameter("enabled")));
+				user.setRole("User");	
+				//logger.info("Enabled: "+Boolean.parseBoolean(request.getParameter("enabled")));
+				//user.setEnabled(Boolean.parseBoolean(request.getParameter("enabled")));
+				user.setEnabled(true);
 				repository.save(user);
 		        model.put("user", user);
 			}else
@@ -65,6 +70,52 @@ public class UserService extends BaseService<User> {
 	    }
 		 
 		return ok(model);
+	}
+	
+	/**
+	 * Created by Syarif Hidayat on 25/04/2019.
+	*/
+	/*This service use to update user data from frontend WEB UI*/
+	@RequestMapping(value="/user/edit", method=RequestMethod.PATCH, consumes = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Map> UpdateUser(@RequestBody User user) {
+
+		Map<Object, Object> model = new HashMap<>();
+		logger.info("Id from Client: "+user.getId());
+		User userdata = repository.findById(user.getId()).get();
+		logger.info("Username: "+ userdata.getUsername());
+		logger.info("FirstName: "+ userdata.getFirstName());
+		
+		if(user.getUsername() != "") {
+			userdata.setUsername(user.getUsername());
+		}
+		
+		if(user.getPassword() != "" && user.getPassword() != userdata.getPassword()) {
+			userdata.setPassword(new BCryptPasswordEncoder().encode(user.getPassword()));
+		}
+		
+		if(user.getFirstName() != "") {
+			userdata.setFirstName(user.getFirstName());
+		}
+		
+		if(user.getLastName() != "") {
+			userdata.setLastName(user.getLastName());
+		}
+		
+		if(user.getEmail() != "") {
+			userdata.setEmail(user.getEmail());
+		}
+		
+		try {		
+			repository.save(userdata);
+		    model.put("user", userdata);
+					                
+	    } catch (Exception e) {
+	    	model.put("Error", e.getMessage());
+	    }
+		 
+		return ok(model);
 	 }
+	
+	
 }
 
